0caa1d14e671ebcba1aeb3b4fcd4cd5b7bbb7b6c
MegavarshanG <166818815+MegavarshanG@users.noreply.github.com>
2025-12-15 22:19:08 +0530

    Time: 5 ms (2.22%) | Memory: 45.7 MB (12.60%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,15 +1,19 @@
-import java.util.*;
 class Solution {
     public boolean search(int[] nums, int target) {
-        int i=-1;
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int a:nums){
-            list.add(a);
-        }
-        Collections.sort(list);
-        i=Collections.binarySearch(list,target);
-        if(i>=0){
-            return true;
+        Arrays.sort(nums);
+        int str=0;
+        int end =nums.length-1;
+        while(str<=end){
+            int  mid=str+(end-str)/2;
+            if(target<nums[mid]){
+                end=mid-1;
+            }
+            else if(target>nums[mid]){
+                str=mid+1;
+            }
+            else{
+                return true;
+            }
         }
         return false;
     }
